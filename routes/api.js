43be500e5271a7aab9a2bea65161704ec9970b5f
crosenau <rosenauc74@hotmail.com>
2019-01-31 00:11:05 -0800

    setup database connection

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,15 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+const expect = require('chai').expect;
+const MongoClient = require('mongodb');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
-module.exports = function (app) {
+module.exports = (app, db) => {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get((req, res) => {
+      // iex trading API: https://api.iextrading.com/1.0/stock/aapl/price
+      console.log(req.query);
     });
     
 };
